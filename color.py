8af7e5e5e05ed407efb3031fcfa552b4ab5b0b77
Chris Hyman <chris@wugs.me>
2020-03-08 23:50:02 -0700

    counting colors

--- color.py
+++ color.py
@@ -1,4 +1,4 @@
-from util import only1
+from util import only1, morethan1, exactly2
 
 class Color:
     def __init__(self, w=False, u=False, b=False, r=False, g=False):
@@ -16,7 +16,15 @@ class Color:
             self.monocolored = True
         else:
             self.monocolored = False
-        if
+        if morethan1(self.colors):
+            self.multicolored = True
+        else:
+            self.multicolored = False
+        if exactly2(self.colors):
+            self.colorpair = True
+            self.guild = self.get_guild()
+        else:
+            self.colorpair = False
 
     def assign_colors(self):
         self.colors = [self.white, self.blue, self.black, self.red, self.green]
--- util.py
+++ util.py
@@ -7,3 +7,17 @@ def only1(lst):
             else:
                 found_true = True
     return found_true
+
+def morethan1(lst):
+    x = 0
+    for b in lst:
+        if b:
+            x += 1
+    return x > 1
+
+def exactly2(lst):
+    x = 0
+    for b in lst:
+        if b:
+            x += 1
+    return x == 2
